ce04397436a951c9d39a7e2f1351c2c664485703
Nishant Pandey <59759332+nishant2106@users.noreply.github.com>
2022-10-05 19:28:44 +0530

    cors added

--- routes/cors.js
+++ routes/cors.js
@@ -2,7 +2,11 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 
-const whitelist = ["http://localhost:3000", "https://localhost:3443"];
+const whitelist = [
+  "http://localhost:3000",
+  "https://localhost:3443",
+  "https://bloodbook-nishant.netlify.app/",
+];
 var corsOptionsDelegate = (req, callback) => {
   var corsOptions;
   if (whitelist.indexOf(req.header("Origin")) !== -1) {
